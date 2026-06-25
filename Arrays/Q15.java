592ae1b24b35bec8bcc17b28ad9f33c816c1f852
Nitin Sharma <nitinsharma9144063@gmail.com>
2026-06-25 22:01:32 +0530

    .

---
+++ Arrays/Q15.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Q15 {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length; i >0 ; i++) {
+            
+        }
+        return digits;
+    }
+    public static void main(String[] args) {
+        int[] arr={4,3,2,1};
+        System.out.println(Arrays.toString(arr));
+    }
+}
