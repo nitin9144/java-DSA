32258cd2d0d5d321d8ca4a693b124c6707589e2d
Nitin Sharma <nitinsharma9144063@gmail.com>
2026-06-29 15:21:43 +0530

    finish Q18

--- Arrays/Q18.java
+++ Arrays/Q18.java
@@ -2,24 +2,20 @@ package Arrays;
 
 public class Q18 {
      public static boolean canJump(int[] nums) {
-        int i=0;
-        while(i<nums.length) {
-            i++;
-            for (int j = 0; j < nums[i]; j++) {
-                i++;
-            }
-            if(i==nums.length-1){   
-                return true;
-            }else{
+        int maxreach=0;
+        for (int i = 0; i < nums.length-1; i++) {
+            if (i>maxreach) {
                 return false;
             }
-
+            maxreach=Math.max(maxreach,i+nums[i]);
         }
-        
+        if (maxreach>=nums.length-1) {
+            return true;
+        }
         return false;
     }
     public static void main(String[] args) {
-        int[] arr= {2,3,1,1,4};
+        int[] arr= {3,2,1,0,4};
         System.out.println(canJump(arr));
     }
 }
