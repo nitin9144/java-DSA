14f0b5f1ef301c061cd4b95c5175b036b539e2ec
Nitin Sharma <nitinsharma9144063@gmail.com>
2026-07-07 22:32:51 +0530

    a

---
+++ BinarySearch_Lecture_Question/Binarysearch_in_infinite_array.java
@@ -0,0 +1,10 @@
+package BinarySearch_Lecture_Question;
+
+public class Binarysearch_in_infinite_array {
+    public static int binarysearch(){
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
