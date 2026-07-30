539d8984287d7fcca3ba803e742683a6e8eae8f4
Nitin Sharma <nitinsharma9144063@gmail.com>
2026-07-30 18:21:15 +0530

    bb

--- Sorting/bubblesort.java
+++ Sorting/bubblesort.java
@@ -1,8 +1,30 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class bubblesort {
-    
+
+    static int[] bubblesort(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped=false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,4};
+        System.out.println(Arrays.toString(bubblesort(arr)));
     }
 }
