8c5b494664a4f125ddcc2feddc351a36009f3645
Nitin Sharma <nitinsharma9144063@gmail.com>
2026-08-05 22:46:00 +0530

    correction

--- Sorting/Selectionsort.java
+++ Sorting/Selectionsort.java
@@ -3,20 +3,24 @@ package Sorting;
 import java.util.Arrays;
 
 public class Selectionsort {
-    static void selectionsort(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            int smallest=arr[i];
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[j]<smallest) {
-                    smallest=arr[j];
+
+    static void selectionsort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[smallest]) {
+                    smallest = j;
                 }
             }
-            arr[i]=smallest;
-            
+            int temp=arr[i];
+            arr[i] = arr[smallest];
+            arr[smallest]=temp;
+
         }
     }
+
     public static void main(String[] args) {
-        int[] arr={3,2,7,4};
+        int[] arr = {3, 2, 7, 4,7,7,9,12,1,4,14,21};
         selectionsort(arr);
         System.out.println(Arrays.toString(arr));
     }
