5ad40be519a4f0161e9d194aae8bdcbeadd8a68d
Nitin Sharma <nitinsharma9144063@gmail.com>
2026-08-07 01:57:41 +0530

    cyclicsort duplicates

--- Sorting/CyclicSort.java
+++ Sorting/CyclicSort.java
@@ -3,26 +3,42 @@ package Sorting;
 import java.util.Arrays;
 
 public class CyclicSort {
-    public static void swap(int[] arr,int a,int b) {
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
-    public static void  CyclicSort(int[] arr){
+
+    // public static void CyclicSort(int[] arr) {
+    //     // for (int i = 0; i < arr.length-1; i++) {
+    //     int i = 0;
+    //     while (i < arr.length) {
+    //         int correct_index = arr[i] - 1;
+    //         if (i == correct_index) {
+    //             i++;
+    //         } else {
+    //             swap(arr, i, correct_index);
+    //         }
+    //     }
+    // }
+
+    public static void CyclicSort(int[] arr) {
         // for (int i = 0; i < arr.length-1; i++) {
-        int i=0;
-        while(i<arr.length){
-            int correct_index=arr[i]-1;
-            if(i==correct_index){
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
                 i++;
-            }else{
-                swap(arr, i, correct_index);
             }
         }
     }
+
     public static void main(String[] args) {
-        int[] arr={2,5,4,3,1};
+        int[] arr = {2, 5, 4, 4, 1};
         CyclicSort(arr);
-System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
     }
 }
