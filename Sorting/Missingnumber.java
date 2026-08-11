d70c61bc4ed220e524c64466e560a1b2bacb0fe5
Nitin Sharma <nitinsharma9144063@gmail.com>
2026-08-11 23:50:51 +0530

    missing

--- Sorting/Missingnumber.java
+++ Sorting/Missingnumber.java
@@ -1,7 +1,5 @@
 package Sorting;
 
-import java.util.Arrays;
-
 public class Missingnumber {
 
     public static void swap(int[] nums, int a, int b) {
@@ -10,26 +8,45 @@ public class Missingnumber {
         nums[b] = temp;
     }
 
-    public static void missingNumber(int[] nums) {
+    public static int missingNumber(int[] nums) {
         int i = 0;
         while (i < nums.length) {
-            int correct = nums[i];  
-            if (correct>=nums.length) {
-                i++;
-                continue;
-            }
-            if (nums[i] != nums[correct]) {
+            int correct = nums[i];
+            // if (correct >= nums.length) {
+            //     i++;
+            //     continue;
+            // }
+            if (nums[i]<nums.length && nums[i] != nums[correct]) {
                 swap(nums, i, correct);
-                
+
             } else {
                 i++;
             }
         }
+        // int j = 0;
+        // while (j < nums.length) {
+        //     int correct = nums[j];
+
+        //     if (nums[j] != correct) {
+        //         return correct;
+        //     } 
+        //         j++;
+        // }    
+        // return j-1;
+        for (int j = 0; j < nums.length; j++) {
+            int correct = nums[j];
+
+            if (nums[j] !=j){
+                return j;
+            }
+        }
+        return nums.length;
     }
 
     public static void main(String[] args) {
-        int[] nums = {3, 1, 0};
-        missingNumber(nums);
-System.out.println(Arrays.toString(nums));
+        int[] nums = {0, 1, 2,3, 4, 5};
+        // missingNumber(nums);
+// System.out.println(Arrays.toString(nums));
+        System.out.println(missingNumber(nums));
     }
 }
