840873d5674ccf4cd0e70bffd2293fa1a957058f
nitin9144 <nitinsharma9144063@gmail.com>
2026-08-26 22:04:14 +0530

    a

---
+++ Sorting/Find_all_duplicates.java
@@ -0,0 +1,38 @@
+package Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Find_all_duplicates {
+    class Solution {
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static List<Integer> findDuplicates(int[] nums) {
+        int i = 0;
+        while ( i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+
+            } else {
+                i++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+           
+
+            if (nums[j] != j+1) {
+
+                ans.add(nums[j]);
+            }
+        }
+        return ans;
+
+    }
+}
+}
